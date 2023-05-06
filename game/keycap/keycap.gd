0bc3998cee5ccf327f864cb5de73bed6ec6f601e
Koen Heltzel <koenheltzel@gmail.com>
2023-05-07 00:11:30 +0200

    Fixed coloring active keys

--- game/keycap/keycap.gd
+++ game/keycap/keycap.gd
@@ -9,10 +9,10 @@ extends Node3D
 
 func _ready():
 	self.label_3d.text = self.letter
-	self.mesh_instance_3d.material_override = self.mesh_instance_3d.mesh.surface_get_material(0).duplicate()
+	self.mesh_instance_3d.material_override = self.mesh_instance_3d.mesh.surface_get_material(0).duplicate(true)
 	(self.mesh_instance_3d.material_override as StandardMaterial3D).backlight_enabled = false
+	(self.mesh_instance_3d.material_override as StandardMaterial3D).backlight = Color.CORNFLOWER_BLUE
 
 func _input(event) -> void:
-	(self.mesh_instance_3d.material_override as StandardMaterial3D).backlight_enabled = event.is_action_pressed(self.letter)
-	if event.is_action_pressed(self.letter):
-		(self.mesh_instance_3d.material_override as StandardMaterial3D).backlight = Color.CORNFLOWER_BLUE
+	(self.mesh_instance_3d.material_override as StandardMaterial3D).backlight_enabled = Input.is_action_pressed(self.letter)
+	self.position.y = -0.1 if Input.is_action_pressed(self.letter) else 0
